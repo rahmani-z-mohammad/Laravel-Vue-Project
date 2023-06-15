09af1065816a5ea0f6fdeef11441d160a47142e9
Mohammad Z Rahmani <rahmani.zalmai@gmail.com>
2023-06-15 22:44:15 +0200

    Displayin flash message

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -4,14 +4,38 @@
 <Link href="/hello"> Show Page</Link>
 <!--<div>The page time with: {{ timer }}</div>-->
 
+  <div v-if="flashSuccess" class="success">
+  {{ flashSuccess }}
+  </div>
+
 <!-- slot render content, if no content, then render Defult -->
 <slot>Default</slot>
 </template>
 
 <script setup>
-  import {Link} from '@inertiajs/vue3';
+//with usePage function we can have access to all data that we share through the Inertia Laravel Middleware
+  import {Link, usePage} from '@inertiajs/vue3';
+  import { computed } from '@vue/reactivity';
   //import { ref } from 'vue';
+
+
+  // page.props.flash.success
+  const page = usePage()
+  const flashSuccess = computed(
+    () => page.props.flash.success
+  )
+  
 
   //const timer = ref(0)
   //setInterval(() => timer.value ++, setTimeout,1000)
 </script>
+
+<style scoped>
+
+.success{
+
+  background-color: green;
+  color: white;
+}
+
+</style>
