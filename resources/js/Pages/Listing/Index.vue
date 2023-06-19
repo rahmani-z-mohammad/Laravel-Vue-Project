ccccb5505a5417fe4300f9f09e74916a47e60d5f
Mohammad Z Rahmani <rahmani.zalmai@gmail.com>
2023-06-19 21:43:53 +0200

    UI Components and Grid With Responsive Design

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,6 +1,7 @@
 <template>
  
- <div v-for="listing in listings" :key="listing.id">
+ <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+  <Box v-for="listing in listings" :key="listing.id">
     <div>
       <Link :href="route('listing.show',{listing: listing.id})">
        <ListingAddress :listing="listing"></ListingAddress>
@@ -13,13 +14,15 @@
       <!-- Default method in Inertia Link is Get, But we can use another like Delete Methos-->
       <Link :href="route('listing.destroy',{listing: listing.id})" method="DELETE" as="button">Delete</Link>
     </div>
-</div>
+  </Box>
+ </div>
 
 </template>
 
 <script setup>
 
 import ListingAddress from '@/Components/ListingAddress.vue';
+import Box from '@/Components/UI/Box.vue';
 import { Link } from '@inertiajs/vue3';
 
 defineProps({
