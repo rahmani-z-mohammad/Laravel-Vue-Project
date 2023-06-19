993925482f54159a989f66bd012419757fa8bc74
Mohammad Z Rahmani <rahmani.zalmai@gmail.com>
2023-06-19 22:34:53 +0200

    Price Formatting Component

---
+++ resources/js/Components/ListingSpace.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <span class="font-bold">{{ listing.beds }}</span> Bed <span class="text-gray-400"> | </span>
+      <span class="font-bold">{{ listing.baths }}</span> Bath <span class="text-gray-400"> | </span>
+      <span class="font-bold">{{ listing.area }}</span> Area m²
+    </div>
+</template>
+
+<script setup>
+defineProps({
+listing: Object
+})
+</script>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -4,7 +4,9 @@
   <Box v-for="listing in listings" :key="listing.id">
     <div>
       <Link :href="route('listing.show',{listing: listing.id})">
-       <ListingAddress :listing="listing"></ListingAddress>
+       <Price :price="listing.price" class="text-2xl font-bold"></Price>
+       <ListingSpace :listing="listing" class="text-lg"></ListingSpace>
+       <ListingAddress :listing="listing" class="text-gray-500"></ListingAddress>
       </Link>
     </div>
     <div>
@@ -22,6 +24,8 @@
 <script setup>
 
 import ListingAddress from '@/Components/ListingAddress.vue';
+import ListingSpace from '@/Components/ListingSpace.vue';
+import Price from '@/Components/Price.vue';
 import Box from '@/Components/UI/Box.vue';
 import { Link } from '@inertiajs/vue3';
 
