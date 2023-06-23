1a0228fe5cd8eb039765a76383366268ac571ae4
Mohammad Z Rahmani <rahmani.zalmai@gmail.com>
2023-06-23 23:19:42 +0200

    Logout Users

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,8 +15,10 @@ class AuthController extends Controller
 
     public function store(Request $request){
 
-        // Auth::attempt Laravel attempt to find such a user and return true oder false
-        // second parametr is remember me, for now true means always login untel manualy logout by user
+        /*
+         Auth::attempt Laravel attempt to find such a user and return true oder false
+        second parametr is remember me, for now true means always login untel manualy logout by user
+        */
         if(!Auth::attempt($request -> validate([
             'email' => 'required|string|email',
             'password' => 'required|string'
@@ -28,13 +30,19 @@ class AuthController extends Controller
 
         $request -> session() -> regenerate();
 
-        // redirect to the login page in create function and this handel automaticlly by laravel
-        //also we can put our own route like another page in intended function
+        /*
+        redirect to the login page in create function and this handel automaticlly by laravel
+        also we can put our own route like another page in intended function
+        */
         return redirect() -> intended('/listing');
  
     }
 
-    public function destroy(){
+    public function destroy(Request $request){
+        Auth::logout();
 
+        $request -> session() -> invalidate();
+        $request -> session() -> regenerateToken();
+        return redirect()->route('listing.index');
     }
 }
