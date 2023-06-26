b21b3a695732e01f2ce4e13303ecc497988dcc4d
Mohammad Z Rahmani <rahmani.zalmai@gmail.com>
2023-06-26 21:50:59 +0200

    Filtring Form

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,5 +1,6 @@
 <template>
- 
+    <Filters/>
+
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
 
     <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
@@ -17,6 +18,7 @@
 
 import Pagination from '@/Components/UI/Pagination.vue';
 import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
+import Filters from '@/Pages/Listing/Index/Components/Filters.vue';
 
 defineProps({
     // object while Listing controller with pagination return an object
