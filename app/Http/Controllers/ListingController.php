fbb0be8eab6375cf563b68fbd416259ccd9a871b
Mohammad Z Rahmani <rahmani.zalmai@gmail.com>
2023-06-27 11:25:55 +0200

    SW - Backend Filtering Data Queries in Eloqouent

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -30,40 +30,36 @@ class ListingController extends Controller
             'priceFrom', 'priceTo', 'beds', 'baths', 'areaFrom', 'areaTo'
         ]);
 
-        $query = Listing::orderByDesc('created_at');
-
-        //?? false by this way when some filter fields are emty then we dont have error by clicking pagination
-        if($filters['priceFrom'] ?? false){
-            $query->where('price', '>=', $filters['priceFrom']);
-        }
-
-        if($filters['priceTo'] ?? false){
-            $query->where('price', '<=', $filters['priceTo']);
-        }
-
-        if($filters['beds'] ?? false){
-            $query->where('beds', $filters['beds']);
-        }
-
-        if($filters['baths'] ?? false){
-            $query->where('baths', $filters['baths']);
-        }
-
-        if($filters['areaFrom'] ?? false){
-            $query->where('areaFrom', '>=', $filters['areaFrom']);
-        }
-
-        if($filters['areaTo'] ?? false){
-            $query->where('areaTo', '<=', $filters['areaTo']);
-        }
+        /*
+        - The when method allows youe to conditionally build queries.
+        - ?? false by this way when some filter fields are emty then we dont have error by clicking pagination
+        */
 
         return inertia(
             'Listing/Index',
             [
                 'filters' => $filters,
-                'listings'=> $query->paginate(10)
-                    ->withQueryString()
-                    // withQueryString() we dont lose the url filter data when clicking on pages
+                'listings'=> Listing::orderByDesc('created_at')
+                ->when(
+                    $filters['priceFrom'] ?? false,
+                    fn($query, $value)=>$query->where('price', '>=', $value)
+                )->when(
+                    $filters['priceTo'] ?? false,
+                    fn($query, $value)=>$query->where('price', '<=', $value)
+                )->when(
+                    $filters['beds'] ?? false,
+                    fn($query, $value)=>$query->where('beds', (int)$value < 6 ? '=' : '>=', $value)
+                )->when(
+                    $filters['baths'] ?? false,
+                    fn($query, $value)=>$query->where('baths', (int)$value < 6 ? '=' : '>=', $value)
+                )->when(
+                    $filters['areaFrom'] ?? false,
+                    fn($query, $value)=>$query->where('area', '>=', $value)
+                )->when(
+                    $filters['areaTo'] ?? false,
+                    fn($query, $value)=>$query->where('area', '<=', $value)
+                )->paginate(10)->withQueryString()
+                // withQueryString() we dont lose the url filter data when clicking on pages
             ]   
     
             );
